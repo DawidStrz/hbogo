61f10063689f0769feca0251d5f5de9756828b0e
Dawid Strzalkowski <dawidstrzalkowski@gmail.com>
2020-02-25 00:00:50 +0100

    reduction of execution time

--- conf.js
+++ conf.js
@@ -1,18 +1,17 @@
 exports.config = {
-    directConnect: true,
-    framework: 'jasmine2',
-    // seleniumAddress: 'http://localhost:4444/wd/hub',
-    baseUrl: 'https://hbogo.pl',
-    specs: ['spec/hbogo.spec.js'],
-    capabilities: {
-      browserName: 'chrome'
-    },
-    jasmineNodeOpts: {
-        defaultTimeoutInterval: 30000
-      },
-    onPrepare() {
-        browser.manage().window().maximize();
-        browser.ignoreSynchronization = true;
-        browser.driver.manage().timeouts().pageLoadTimeout(40000);
-      }
+  directConnect: true,
+  framework: 'jasmine2',
+  baseUrl: 'https://hbogo.pl',
+  specs: ['spec/hbogo.spec.js'],
+  capabilities: {
+    browserName: 'chrome'
+  },
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 20000
+  },
+  onPrepare() {
+    browser.manage().window().maximize();
+    browser.ignoreSynchronization = true;
+    browser.driver.manage().timeouts().pageLoadTimeout(30000);
   }
+}
--- pages/settingsPO.js
+++ pages/settingsPO.js
@@ -1,30 +1,35 @@
-var settingsPO = function(){
-    var base = require('./basePagePO.js');    
+var settingsPO = function () {
+    var base = require('./basePagePO.js');
 
-        this.openSettingsButton = element(by.id('nickname'));
+    this.openSettingsButton = element(by.id('nickname'));
+    this.subscriptionManagement = element(by.id('gw_settings_sidebar_subscription'));
+    this.subscriptionCancelButton = element(by.id('gw_subscription_cancel_subscription_button'));
 
-        this.openSettings = function(){
-            base.isElementVisible(this.openSettingsButton);
-            return this.openSettingsButton.click();
-        }
-
-        this.subscriptionManagement = element(by.id('gw_settings_sidebar_subscription'));
-
-        this.subscriptionManagementOpen = function(){
-            base.switchFrame('gatewayFrame');
-            base.isElementVisible(this.subscriptionManagement);
-            return this.subscriptionManagement.click();
-        }
-    
-        this.subscriptionCancel = function(){
-            return element(by.id('gw_subscription_cancel_subscription_button')).click();
-        }
-        
-        this.confirmSubscriptionCancel = function(){
-            let confirmCancelButton = element(by.css('span.btn.primary.selected.btn-type-l.gateway-subpage-cancel'));
-            browser.actions().mouseMove(element(by.css('.footer'))).perform().then(function (){
-                return confirmCancelButton.click();
-            })
-        }
+    this.openSettings = function () {
+        base.isElementVisible(this.openSettingsButton);
+        return this.openSettingsButton.click();
     }
-    module.exports = new settingsPO();
+
+    this.subscriptionManagementOpen = function () {
+        base.switchFrame('gatewayFrame');
+        base.isElementVisible(this.subscriptionManagement);
+        return this.subscriptionManagement.click();
+    }
+
+    this.subscriptionCancel = function () {
+        base.isElementVisible(this.subscriptionCancelButton);
+        return this.subscriptionCancelButton.click();
+    }
+
+    this.confirmSubscriptionCancel = function () {
+        var confirmCancelButton = element(by.css('span.btn.primary.selected.btn-type-l.gateway-subpage-cancel'));
+        browser.actions().mouseMove(element(by.css('.footer'))).perform().then(function () {
+            return confirmCancelButton.click();
+        })
+    }
+
+    this.confirmSubscriptionCancelInvisible = function () {
+        base.isElementInvisible(confirmCancelButton);
+    }
+}
+module.exports = new settingsPO();
